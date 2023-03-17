001583aba177ff5e9f59c8f901e8edddc5d02c4c
Florent CARRE <florent.carre@free.fr>
2023-03-18 00:16:10 +0100

    move logger out of common

--- internal/common/logger/logger.go
+++ internal/common/logger/logger.go
@@ -1,5 +1,5 @@
 // logger shared between files of the package groom
-package common
+package logger
 
 import (
 	"os"
@@ -9,16 +9,15 @@ import (
 )
 
 func InitLogger() {
+	// Configure logger to write to the file and include caller information
+	log.Logger = log.With().Caller().Logger()
+	log.Logger = log.With().CallerWithSkipFrameCount(1).Logger()
 	// Create a file for logging
-	file, err := os.OpenFile("./log/poker.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	file, err := os.OpenFile("./poker.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
 	if err != nil {
 		log.Fatal().Err(err).Msg("Failed to open log file")
 	}
-
-	// Configure logger to write to the file and include caller information
 	log.Logger = log.Output(file)
-	log.Logger = log.With().Caller().Logger()
-	log.Logger = log.With().CallerWithSkipFrameCount(1).Logger()
 
 	// Set global log level to debug
 	zerolog.SetGlobalLevel(zerolog.DebugLevel)
