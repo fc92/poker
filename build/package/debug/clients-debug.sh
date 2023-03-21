a43d3b603610b05458777d3cd6dcf500c549203d
Florent CARRE <florent.carre@free.fr>
2023-03-21 23:51:15 +0100

    dlv without stdin

--- build/package/debug/clients-debug.sh
+++ build/package/debug/clients-debug.sh
@@ -5,6 +5,6 @@ SERVER=localhost:8080
 # start server in the background
 /poker server -websocket $SERVER & 
 
-/dlv debug /go/src/cmd -r stdin:/tmp/stdin.txt --headless --listen=:$DEBUG_PORT --api-version=2 -- client -websocket $SERVER
+/dlv debug /go/src/cmd --headless --listen=:$DEBUG_PORT --api-version=2 -- client -websocket $SERVER
 
 # use kubect attach -it to access the UI
