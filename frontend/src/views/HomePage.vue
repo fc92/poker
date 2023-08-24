133ee9c9a4a2efe86d1f1b318962df84a283209e
Florent CARRE <{ID}+{username}@users.noreply.github.com>
2023-08-24 23:01:24 +0200

    init ionic vue front with slider

---
+++ frontend/src/views/HomePage.vue
@@ -0,0 +1,99 @@
+<template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-title>Blank</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Blank</ion-title>
+        </ion-toolbar>
+      </ion-header>
+
+      <div id="container">
+        <strong>Ready to create an app?</strong>
+        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer"
+            href="https://ionicframework.com/docs/components">UI Components</a></p>
+      </div>
+    </ion-content>
+    <ion-content>
+      <ion-list>
+        <ion-item>
+          <ion-checkbox slot="start"></ion-checkbox>
+          <ion-label>
+            <h1>Create Idea</h1>
+            <ion-note>Run Idea By Brandy</ion-note>
+          </ion-label>
+          <ion-badge color="success" slot="end"> 5 Days </ion-badge>
+        </ion-item>
+      </ion-list>
+    </ion-content>
+    <ion-content>
+      <ion-icon :ios="logoApple" :md="logoAndroid"></ion-icon>
+    </ion-content>
+    <ion-content>
+      <ion-fab vertical="bottom" horizontal="end" slot="fixed">
+        <ion-fab-button @click="() => router.push('/new')">
+          <ion-icon :icon="add"></ion-icon>
+        </ion-fab-button>
+      </ion-fab>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import {
+  IonBadge,
+  IonCheckbox,
+  IonContent,
+  IonFab,
+  IonFabButton,
+  IonHeader,
+  IonIcon,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonNote,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/vue';
+import { add, logoAndroid, logoApple } from 'ionicons/icons';
+import { useRouter } from 'vue-router';
+const router = useRouter();
+
+
+</script>
+
+<style scoped>
+#container {
+  text-align: center;
+
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+#container strong {
+  font-size: 20px;
+  line-height: 26px;
+}
+
+#container p {
+  font-size: 16px;
+  line-height: 22px;
+
+  color: #8c8c8c;
+
+  margin: 0;
+}
+
+#container a {
+  text-decoration: none;
+}
+</style>
---
+++ frontend/src/views/NewItem.vue
@@ -0,0 +1,50 @@
+<template>
+    <swiper-container :slides-per-view="3" :space-between="spaceBetween" :centered-slides="true"
+        :pagination="{ hideOnClick: true }" :breakpoints="{ 768: { slidesPerView: 3 } }" @progress="onProgress"
+        @slidechange="onSlideChange">
+        <swiper-slide v-for="value in fibonacciValues" :key="value.num" :style="{ backgroundColor: value.color }">
+            {{ value.num }}
+        </swiper-slide>
+    </swiper-container>
+</template>
+
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import { register } from 'swiper/element/bundle';
+const fibonacciValues: { num: number, color: string }[] = [
+    { num: 1, color: '#f4f4f4' },
+    { num: 1, color: '#262626' },
+    { num: 2, color: '#0f62fe' },
+    { num: 3, color: '#198038' },
+    { num: 5, color: '#007d79' },
+    { num: 8, color: '#1192e8' },
+    { num: 13, color: '#8a3ffc' },
+    { num: 21, color: '#da1e28' }
+];
+
+register();
+
+export default defineComponent({
+    setup() {
+        const spaceBetween = ref<number>(10);
+
+        const onProgress = (e: any) => {
+            const [swiper, progress] = e.detail;
+            console.log(progress);
+        };
+
+        const onSlideChange = (e: any) => {
+            console.log('slide changed');
+        }
+
+        return {
+            spaceBetween,
+            onProgress,
+            onSlideChange,
+            fibonacciValues
+        };
+    }
+
+});
+</script>
