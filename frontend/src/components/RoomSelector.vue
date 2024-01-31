35cda9f4f3f892c682624451fbc1c1be109cb051
Florent CARRE <{ID}+{username}@users.noreply.github.com>
2024-02-01 00:12:22 +0100

    fix room list display

--- frontend/src/components/RoomSelector.vue
+++ frontend/src/components/RoomSelector.vue
@@ -11,23 +11,53 @@
             </ion-list>
         </ion-radio-group>
     </ion-list>
-    <ion-button @click="selectRoom">Select room</ion-button>
+    <ion-button v-show="roomList.length > 0" @click="selectRoom">Select room</ion-button>
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from 'vue';
+import { onBeforeMount, ref } from 'vue';
 import { useStore } from 'vuex';
 import { IonList, IonListHeader, IonItem, IonRadioGroup, IonRadio, IonButton } from '@ionic/vue';
 
-const store = useStore();
 const emit = defineEmits();
+var roomList = ref<string[]>([]);
+var selectedRoom = ref<string>('');
 
-store.dispatch('getRoomList');
-var roomList: string[] = computed(() => store.state.roomList).value;
+onBeforeMount(async () => {
+    const store = useStore();
+    try {
+        store.dispatch('getRoomList');
+        await waitForRoomList();
+        roomList.value = store.state.roomList;
+        selectedRoom.value = roomList.value[0];
+        console.log('room list:', roomList.value);
+    } catch (error) {
+        console.error('Failed to load room list:', error);
+    }
 
-const selectedRoom = ref(roomList[0]);
+    async function waitForRoomList() {
+        const maxAttempts = 10;
+        let attempts = 0;
+
+        while (store.state.roomList.length === 0 && attempts < maxAttempts) {
+            await new Promise(resolve => setTimeout(resolve, 1000)); // Pause d'une seconde entre les tentatives
+            attempts++;
+        }
+
+        if (store.state.roomList.length === 0) {
+            console.error('La liste des salles n\'a pas été chargée après plusieurs tentatives.');
+        } else {
+            roomList.value = store.state.roomList;
+            console.log('La liste des salles a été chargée:', roomList.value);
+        }
+    }
+});
+
+
+
 const selectRoom = () => {
-    emit('update:room', selectedRoom);
+    emit('update:room', selectedRoom.value);
 };
+
 
 </script>
