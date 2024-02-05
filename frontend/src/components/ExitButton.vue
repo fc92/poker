e54b5b88b6d58f0fb258e222a6eda98a1d275f66
Florent CARRE <{ID}+{username}@users.noreply.github.com>
2024-02-05 21:28:21 +0100

    exit navigates to home page

--- frontend/src/components/ExitButton.vue
+++ frontend/src/components/ExitButton.vue
@@ -10,10 +10,14 @@
 import { IonButton, IonIcon } from '@ionic/vue';
 import { exit } from 'ionicons/icons';
 import { useStore } from 'vuex';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
 
 const store = useStore();
 const handleExitClick = () => {
     store.dispatch('handleExitClick');
+    router.push('/home');
 };
 
 </script>
