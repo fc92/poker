bf977659d3c867e6b744d7729a2738e0ee144cb5
Florent CARRE <{ID}+{username}@users.noreply.github.com>
2024-02-28 23:48:18 +0100

    link to project on github

--- frontend/src/components/ExitButton.vue
+++ frontend/src/components/ExitButton.vue
@@ -3,6 +3,14 @@
         <ion-button @click="handleExitClick">
             <ion-icon :icon="exit"></ion-icon>
         </ion-button>
+        <ion-toolbar>
+            <ion-buttons slot="end">
+                <!-- Add a button to open the GitHub URL in a new tab or window -->
+                <ion-button @click="openGitHubInNewTab" size="small">
+                    Project on GitHub
+                </ion-button>
+            </ion-buttons>
+        </ion-toolbar>
     </div>
 </template>
       
@@ -19,5 +27,10 @@ const handleExitClick = () => {
     store.dispatch('handleExitClick');
     router.push('/home');
 };
+
+const openGitHubInNewTab = () => {
+    window.open('https://github.com/fc92/poker', '_blank');
+};
+
 
 </script>
